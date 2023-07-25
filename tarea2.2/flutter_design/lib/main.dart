0c6629ef568687f439bdba367794f4fb0406a833
jimmyov29 <82368446+jimmyov29@users.noreply.github.com>
2023-07-24 21:59:25 -0600

    done

---
+++ tarea2.2/flutter_design/lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_design/src/pages/basic_design.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      initialRoute: '/',
+      routes: {
+        '/': (context) => const BasicDesign(),
+      },
+    );
+  }
+}
